95028ecfae7e7eab940643564568151baaf8ebdc
ago19800 <56254637+ago19800@users.noreply.github.com>
2026-02-07 15:58:30 +0100

    Add CentralinoConfigFlow for Centralino integration

--- custom_components/centralino/config_flow.py
+++ custom_components/centralino/config_flow.py
@@ -1,1 +1,14 @@
+from homeassistant import config_entries
+
+DOMAIN = "centralino"
+
+class CentralinoConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        # Non chiediamo nulla all’utente
+        return self.async_create_entry(
+            title="Centralino Manager",
+            data={}
+        )
 
